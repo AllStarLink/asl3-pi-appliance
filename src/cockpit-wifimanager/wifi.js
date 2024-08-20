1bc2d19cbae1e0d8c2e32d3d5e30b03bc7778deb
PCPackrat <32687321+pcpackrat@users.noreply.github.com>
2024-08-20 11:45:49 -0500

    Update wifi.js

--- src/cockpit-wifimanager/wifi.js
+++ src/cockpit-wifimanager/wifi.js
@@ -3,15 +3,23 @@ const output = document.getElementById("output");
 const result = document.getElementById("result");
 const button = document.getElementById("wifi-scan");
 
+let accumulatedOutput = "";  // Variable to accumulate output
+
 function wifi_scan_run() {
     /* global cockpit */
     cockpit.spawn(["nmcli", "device", "wifi", "list"])
-            .stream(wifi_scan_output)
-            .then(wifi_scan_success)
-            .catch(wifi_scan_fail);
+        .stream((data) => {
+            accumulatedOutput += data;  // Accumulate the output
+        })
+        .then(() => {
+            wifi_scan_output(accumulatedOutput);  // Display full output
+            wifi_scan_success();
+        })
+        .catch(wifi_scan_fail);
 
     result.textContent = "";
     output.textContent = "";
+    accumulatedOutput = "";  // Reset accumulated output for next scan
 }
 
 function wifi_scan_success() {
@@ -25,13 +33,13 @@ function wifi_scan_fail() {
 }
 
 function wifi_scan_output(data) {
-	let out_html = `<hr><b>Available Wi-Fi Networks<b><br>`;
-	out_html += `<pre>${data}</pre><hr>`;
-	output.innerHTML = out_html;
+    let out_html = `<hr><b>Available Wi-Fi Networks<b><br>`;
+    out_html += `<pre>${data}</pre><hr>`;
+    output.innerHTML = out_html;
 }
 
 // Connect the button to starting the "ping" process
 button.addEventListener("click", wifi_scan_run);
 
 // Send a 'init' message.  This tells integration tests that we are ready to go
-cockpit.transport.wait(function() { });
+cockpit.transport.wait(function() {});
