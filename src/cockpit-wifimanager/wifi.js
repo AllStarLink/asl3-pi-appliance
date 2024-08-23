0cd1c0351ccc65e4899dce34db8f6c98bc234abc
Jason D. McCormick <jason@mfamily.org>
2024-08-23 10:25:17 -0400

    fixes

--- src/cockpit-wifimanager/wifi.js
+++ src/cockpit-wifimanager/wifi.js
@@ -5,45 +5,45 @@ const setresult = document.getElementById("set-wifi-results");
 
 function wifi_scan_run() {
     /* global cockpit */
-    result.innerHTML = `<span class="wifi-scan-loader"></span>`;
-    cockpit.spawn(["/usr/share/cockpit/wifimanager/wifi-scan.sh"],
-        { superuser: "require" })
-        .stream(wifi_scan_output)
-        .catch(wifi_scan_fail);
+	result.innerHTML = `<span class="wifi-scan-loader"></span>`;
+	cockpit.spawn(["/usr/share/cockpit/wifimanager/wifi-scan.sh"] ,
+		{ superuser: "require" } )
+            .stream(wifi_scan_output)
+            .catch(wifi_scan_fail);
 }
 
 function wifi_scan_fail() {
-    result.innerHTML += `<span class="wifi-scan-error">WiFi scan failed</span>`;
+	result.innerHTML += `<span class="wifi-scan-error">WiFi scan failed</scan>`;
 }
 
 function wifi_scan_output(data) {
-    let out_html = `<b>Available Wi-Fi Networks</b>`;
-    out_html += `<pre>${data}</pre>`;
-    result.innerHTML = out_html;
+	let out_html = `<b>Available Wi-Fi Networks<b>`;
+	out_html += `<pre>${data}</pre>`;
+	result.innerHTML = out_html;
 }
 
 function set_wifi_run() {
     /* global cockpit */
-    setresult.innerHTML = `<span class="wifi-scan-loader"></span>`;
-    const net_name = document.getElementById("wifi-ssid").value;
-    const net_key = document.getElementById("wifi-key").value;
-    cockpit.spawn(["/usr/share/cockpit/wifimanager/wifi-set.sh", net_name, net_key],
-        { superuser: "require" })
-        .stream(set_wifi_output)
-        .catch(set_wifi_fail);
+	result.innerHTML = `<span class="wifi-scan-loader"></span>`;
+	const net_name = document.getElementById("wifi-ssid").value;
+	const net_key = document.getElementById("wifi-key").value;
+	cockpit.spawn(["/usr/share/cockpit/wifimanager/wifi-set.sh", net_name, net_key] ,
+		{ superuser: "require" } )
+            .stream(set_wifi_output)
+            .catch(set_wifi_fail);
 }
 
 function set_wifi_fail() {
-    setresult.innerHTML += `<span class="wifi-scan-error">WiFi set failed</span>`;
+	setresult.innerHTML += `<span class="wifi-scan-error">WiFi set failed</scan>`;
 }
 
 function set_wifi_output(data) {
-    setresult.innerHTML = `<pre>${data}</pre>`;
+	setresult.innerHTML = `<pre>${data}</pre>`;
 }
 
 button.addEventListener("click", wifi_scan_run);
-setbutton.addEventListener("click", set_wifi_run);
+setbutton.addEventListener("click" , set_wifi_run);
 
-// Send an 'init' message. This tells integration tests that we are ready to go
-cockpit.transport.wait(function() {});
 
+// Send a 'init' message.  This tells integration tests that we are ready to go
+cockpit.transport.wait(function() { });
